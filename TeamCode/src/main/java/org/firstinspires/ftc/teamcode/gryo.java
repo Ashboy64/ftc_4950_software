be994dbaa57090d9cf9350497aef06ba985be55e
CarterBi <carterbian32@gmail.com>
2017-09-14 15:27:11 -0700

    First gyro code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gryo.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+@Autonomous(name="First autonomous", group="group")
+@Disabled
+public class gryo extends LinearOpMode{
+    GyroSensor gyro;
+    public void runOpMode () {
+        gyro = hardwareMap.gyroSensor.get("gryo");
+        gyro.calibrate();
+        while(gyro.isCalibrating()){
+
+        }
+        telemetry.addData("heading", gyro.getHeading());
+        telemetry.update();
+    }
+
+}
