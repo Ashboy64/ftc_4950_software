8cac831a8f66aeedadd737a7f64d42765048ec34
aayushiron <aayushbhat2@gmail.com>
2017-09-19 16:20:49 -0700

    pls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aayush_TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aayush_TeleOpTest.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
  * Created by Aayushiron on 9/19/17.
  */
 
-@TeleOp(name="Concept: VuMark Id", group ="Concept")
+@TeleOp(name="end my suffering", group ="Concept")
 @Disabled
 public class Aayush_TeleOpTest extends OpMode {
     DcMotor motorRight;
@@ -32,5 +32,36 @@ public class Aayush_TeleOpTest extends OpMode {
 
         motorRight.setPower(yvert);
         motorLeft.setPower(yvert);
+    }
+
+    public double scaleInput(double dVal)  {
+
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+
+        if (dVal < 0) {
+            dScale = scaleArray[index];
+        } else {
+            dScale = -scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
 }
