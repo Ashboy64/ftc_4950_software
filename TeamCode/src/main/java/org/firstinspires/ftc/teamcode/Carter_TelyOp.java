8d8f43905391bd533d76ec27a0b1952db93a0fc1
CarterBi <carterbian32@gmail.com>
2017-09-19 16:09:48 -0700

    Carter telyop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carter_TelyOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carter_TelyOp.java
@@ -23,8 +23,8 @@ public class Carter_TelyOp extends OpMode{
 
     public void loop(){
         if(gamepad1.left_stick_x != 0){
-            leftMotor.setPower(gamepad1.left_stick_x * 1);
-            rightMotor.setPower(gamepad1.left_stick_x * 1);
+            leftMotor.setPower(scaleInput(gamepad1.left_stick_x * 1));
+            rightMotor.setPower(scaleInput(gamepad1.left_stick_x * 1));
         }else{
             leftMotor.setPower(0);
             rightMotor.setPower(0);
@@ -34,4 +34,37 @@ public class Carter_TelyOp extends OpMode{
     public void stop(){
 
     }
-}
+
+    public double scaleInput(double dVal)  {
+
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+
+        if (dVal < 0) {
+            dScale = scaleArray[index];
+        } else {
+            dScale = -scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
+ }
+
