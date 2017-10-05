174239ddedc07bb5ca3f44082ad3ed9c654fe730
aayushiron <aayushbhat2@gmail.com>
2017-10-05 16:31:52 -0700

    pls12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aayush_GyroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aayush_GyroTest.java
@@ -5,7 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by Aayushiron on 9/12/17.
@@ -14,22 +16,35 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
 @Autonomous(name="GyroTest", group="group")
 //@Disabled
 public class Aayush_GyroTest extends LinearOpMode{
-    //DcMotor LeftMotor;
-    //DcMotor RightMotor;
-    GyroSensor gyro;
+    DcMotor LeftMotor;
+    DcMotor RightMotor;
+    //GyroSensor gyro;
     public void runOpMode(){
-        //LeftMotor = hardwareMap.dcMotor.get("motor1");
-        //RightMotor = hardwareMap.dcMotor.get("motor2");
-        gyro = hardwareMap.gyroSensor.get("gyro1");
 
-        gyro.calibrate();
+        ElapsedTime opmodeRunTime = new ElapsedTime();
 
-        while (gyro.isCalibrating() && opModeIsActive()) {
-            telemetry.addData(">", "Calibrating Sensor. Please Wait.");
+
+        while (!isStarted()) {
+            telemetry.addData("time", "%.1f seconds", opmodeRunTime.seconds());
             telemetry.update();
+            idle();
         }
 
-        /* LeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LeftMotor = hardwareMap.dcMotor.get("motor1");
+        RightMotor = hardwareMap.dcMotor.get("motor2");
+
+        RightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //gyro = hardwareMap.gyroSensor.get("gyro1");
+
+        //gyro.calibrate();
+
+        /*while (gyro.isCalibrating() && opModeIsActive()) {
+            telemetry.addData(">", "Calibrating Sensor. Please Wait.");
+            telemetry.update();
+        }*/
+
+         LeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         RightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         LeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -42,14 +57,17 @@ public class Aayush_GyroTest extends LinearOpMode{
         RightMotor.setPower(1.0);
 
         while (LeftMotor.getCurrentPosition()<LeftMotor.getTargetPosition() && RightMotor.getTargetPosition()>RightMotor.getCurrentPosition() && opModeIsActive()) {
-            telemetry.addData(">", gyro.getHeading());
-            telemetry.update();
+           // telemetry.addData(">", gyro.getHeading());
+            //telemetry.update();
         }
+
+        LeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         LeftMotor.setTargetPosition(4000);
         RightMotor.setTargetPosition(4000);
 
-        while (gyro.getHeading() < 90) {
+        /*while (gyro.getHeading() < 90) {
             LeftMotor.setPower(1.0);
             RightMotor.setPower(0.0);
             telemetry.addData(">", gyro.getHeading());
@@ -63,11 +81,11 @@ public class Aayush_GyroTest extends LinearOpMode{
 
         while (LeftMotor.getCurrentPosition()<LeftMotor.getTargetPosition() && RightMotor.getTargetPosition()>RightMotor.getCurrentPosition() && opModeIsActive()) {
             telemetry.addData(">", gyro.getHeading());
-            telemetry.update();*/
+            telemetry.update();
 
         while (opModeIsActive()) {
             telemetry.addData(">", gyro.getHeading());
             telemetry.update();
-        }
+        } */
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by mnawani on 9/28/2017.
@@ -11,8 +12,24 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 @Autonomous(name = "colorSensorTest", group = "group")
 public class ColorSensorTest extends LinearOpMode {
     ColorSensor color_sensor;
+
+    //@Override
+    /*public synchronized void waitForStart() {
+        super.waitForStart();
+    }*/
+
     @Override
     public void runOpMode() {
+
+        ElapsedTime opmodeRunTime = new ElapsedTime();
+
+
+        while (!isStarted()) {
+            telemetry.addData("time", "%.1f seconds", opmodeRunTime.seconds());
+            telemetry.update();
+            idle();
+        }
+
         color_sensor =  hardwareMap.get(ColorSensor.class, "colorSensor1");
         while (opModeIsActive()) {
             color_sensor.enableLed(true);
