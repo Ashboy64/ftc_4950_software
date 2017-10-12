82af40aaebb7a8712ea07a893d0de6b91223eef1
aayushiron <aayushbhat2@gmail.com>
2017-10-12 15:43:41 -0700

    ewrghjewgrjwherwerwe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aayush_TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aayush_TeleOpTest.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
  * Created by Aayushiron on 9/19/17.
  */
 
-@TeleOp(name="tele_op_1_arcade", group ="Concept")
+@TeleOp(name="TeleOp", group ="Concept")
 //@Disabled
 public class Aayush_TeleOpTest extends OpMode {
     DcMotor motorRight;
