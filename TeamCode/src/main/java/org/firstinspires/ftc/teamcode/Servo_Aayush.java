0d6719b41cc7fa6b300787b6fae9672be3c76019
aayushiron <aayushbhat2@gmail.com>
2017-10-16 20:03:42 -0700

    ,,jhdsfkjsdkjfh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_Aayush.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_Aayush.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -15,21 +16,33 @@ public class Servo_Aayush extends LinearOpMode{
     @Override
     public void runOpMode(){
         ElapsedTime opmodeRunTime = new ElapsedTime();
+       // long startTime = System.currentTimeMillis();
 
         while (!isStarted()) {
             telemetry.addData("time", "%.1f seconds", opmodeRunTime.seconds());
             telemetry.update();
             idle();
         }
-
         servo1 = hardwareMap.servo.get("servo1");
-        servo1.setPosition(0);
-        while (servo1.getPosition() != 0 && opModeIsActive()) {
+        servo1.setPosition(0.0);
 
+        while(System.currentTimeMillis() < 5){
+            telemetry.addData("timings", "%0.1f", System.currentTimeMillis());
+            telemetry.update();
         }
-        servo1.setPosition(1);
-        while (servo1.getPosition() != 1 && opModeIsActive()) {
 
+        while (servo1.getPosition() != 0 && opModeIsActive()) {
+            telemetry.addData("GUILLLLLOOTTTIIIINNEE"," ROBESPIERRE");
+            telemetry.update();
+        }
+        telemetry.addData(servo1.getPosition()+ "", "port number");
+        telemetry.update();
+
+        servo1.setPosition(1);
+
+        while(System.currentTimeMillis() < 10){
+            telemetry.addData("timings", "%0.1f", System.currentTimeMillis());
+            telemetry.update();
         }
     }
 }
