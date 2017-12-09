e1de059c176c008157f610545b393e37282d5e88
aayushiron <aayushbhat2@gmail.com>
2017-12-09 12:34:59 -0800

    Changes to Dumb Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DumbAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DumbAutonomous.java
@@ -9,8 +9,6 @@ import com.qualcomm.robotcore.eventloop.opmode.*;
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Dumb ")
 public class DumbAutonomous extends LinearOpMode{
     RobotClassFinalUse robot = new RobotClassFinalUse();
-    double wheel_diameter = 3.54331; //the diameter of the wheels of our robot.
-    double wheel_circumference = Math.PI * wheel_diameter; //the value of π times the wheel diameter
     double distance = Math.sqrt((12 * 12) + (36 * 36));
     @Override
     public void runOpMode() throws InterruptedException {
