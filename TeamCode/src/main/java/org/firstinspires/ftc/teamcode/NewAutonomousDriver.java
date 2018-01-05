8ee27c224bca143a765ef3a4e852c12e88d879f0
CarterBi <carterbian32@gmail.com>
2018-01-04 16:02:49 -0800

    teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAutonomousDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAutonomousDriver.java
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 /**
  * Created by justi on 2018-01-03.
@@ -10,11 +19,51 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  */
 
 public class NewAutonomousDriver {
-    private LinearOpMode opMode;
+    double wheel_diameter = 3.54331; //the diameter of the wheels of our robot.
+    double wheel_circumference = Math.PI * wheel_diameter; //the value of π times the wheel diameter
+    int ticksPerRevolution = (1120 * 60)/96; //the amount of ticks the encoder takes to revolve one wheel
+    double armWaiting = 2.0;
+    public DcMotor leftMotor = null;
+    public DcMotor rightMotor = null;
+    public DcMotor armMotor;
+    ColorSensor colorSensor;
+    GyroSensor gyro;
+    VuforiaLocalizer vuforia;
+    CRServo clampServo;
+    //CRServo jewelServo;
+    int version;
+    DigitalChannel ARM_TOUCH_OPEN;
+    DigitalChannel ARM_TOUCH_CLOSED;
+    LinearOpMode opMode;
+
+
+    HardwareMap hwMap  = null;
+    private ElapsedTime period  = new ElapsedTime();
     //TODO declaration of motors, servos, sensors, etc.
 
     public NewAutonomousDriver(HardwareMap hardwareMap, LinearOpMode opMode) {
-        //TODO hardware initialization
+        // save reference to HW Map
+        this.opMode = opMode;
+        hwMap = hardwareMap;
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        gyro = hardwareMap.gyroSensor.get("gyro");
+        clampServo = hardwareMap.crservo.get("clampServo");
+        colorSensor = hardwareMap.colorSensor.get("colorSensor");
+        //jewelServo = hardwareMap.crservo.get("jewelServo");
+        ARM_TOUCH_OPEN = hardwareMap.get(DigitalChannel.class, "tsOpen");
+        ARM_TOUCH_CLOSED = hardwareMap.get(DigitalChannel.class, "tsClosed");
+        ARM_TOUCH_OPEN.setMode(DigitalChannel.Mode.INPUT);
+        ARM_TOUCH_CLOSED.setMode(DigitalChannel.Mode.INPUT);
+
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        gyro.calibrate();
+
+        while(gyro.isCalibrating()){
+
+        }
     }
 
     /**
@@ -35,7 +84,32 @@ public class NewAutonomousDriver {
     public void drive(double inches) {
         while (opMode.opModeIsActive())
         {
-            //TODO
+            int encoderTicks = (int) ((inches/wheel_circumference) * ticksPerRevolution);
+
+            leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            leftMotor.setTargetPosition(encoderTicks);
+            rightMotor.setTargetPosition(encoderTicks);
+
+            leftMotor.setPower(0.5);
+            rightMotor.setPower(0.5);
+
+            while (leftMotor.getCurrentPosition() < leftMotor.getTargetPosition() && rightMotor.getCurrentPosition() < rightMotor.getTargetPosition() /* && opMode.opModeIsActive()*/) {
+
+            }
+
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
+
+            leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         }
     }
 
@@ -45,7 +119,13 @@ public class NewAutonomousDriver {
     public void openClamp() {
         while (opMode.opModeIsActive())
         {
-            //TODO
+            clampServo.setDirection(DcMotorSimple.Direction.REVERSE);
+            clampServo.setPower(1);
+            while (!ARM_TOUCH_OPEN.getState() && opMode.opModeIsActive()) {
+
+            }
+            clampServo.setPower(0);
+            clampServo.setDirection(DcMotorSimple.Direction.FORWARD);
         }
     }
 
