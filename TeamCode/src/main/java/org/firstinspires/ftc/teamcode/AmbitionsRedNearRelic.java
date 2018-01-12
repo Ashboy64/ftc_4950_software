2744d32baa65a243d9768bfd900bc5bbf0cada39
CarterBi <carterbian32@gmail.com>
2018-01-11 16:32:31 -0800

    generic meme number 95

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmbitionsRedNearRelic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmbitionsRedNearRelic.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 /**
@@ -8,15 +7,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class AmbitionsRedNearRelic extends LinearOpMode {
     NewRobotClassFinal robot =  new NewRobotClassFinal();
+    int trackableViewed;
+    double cryptoBoxWidth = 7.63;
+    double armOffset = 2.8515;
 
     public void runOpMode(){
-
         robot.init(hardwareMap, this);
-        telemetry.addData("ready", "");
-        telemetry.update();
-        waitForStart();
-        scoreJewel();
-
+        robot.gyroTurning(135);
+        trackableViewed = robot.getTargetColumn();
+        robot.gyroTurning(135);
+        robot.drive(24);
+        robot.drive(((cryptoBoxWidth/2) + (trackableViewed * cryptoBoxWidth)) - armOffset);
+        robot.gyroTurning(-90);
+        robot.openClamp();
+        robot.drive(18);
     }
 
     public void scoreJewel(){
@@ -25,24 +29,20 @@ public class AmbitionsRedNearRelic extends LinearOpMode {
 
     private  void jewelTest() {
         robot.setJewelArmPosition(1);
-
 //        try {
 //            wait(1000);
 //        } catch (InterruptedException e) {
 //            telemetry.addData("biggie killed tupac > ", "1");
 //            telemetry.update();
 //        }
-
         scoreJewelTest();
         robot.setJewelArmPosition(0);
-
 //        try {
 //            wait(1000);
 //        } catch (InterruptedException e) {
 //            telemetry.addData("biggie killed tupac > ", "2");
 //            telemetry.update();
 //        }
-
         telemetry.addData("done!", "");
         telemetry.update();
     }
@@ -55,5 +55,4 @@ public class AmbitionsRedNearRelic extends LinearOpMode {
     private boolean red(){
         return true;
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmbitionsRedNotNearRelic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmbitionsRedNotNearRelic.java
@@ -3,25 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 /**
- * Created by Aayushiron on 1/11/18.
+ * Created by yinghuang on 1/11/18.
  */
 
-public class AmbitionsBlueNearRelic extends LinearOpMode {
+public class AmbitionsRedNotNearRelic extends LinearOpMode{
     NewRobotClassFinal robot = new NewRobotClassFinal();
     int trackableViewed;
     double cryptoBoxWidth = 7.63;
     double armOffset = 2.8515;
-
     @Override
     public void runOpMode() throws InterruptedException {
-        robot.init(hardwareMap, this);
         robot.gyroTurning(135);
         trackableViewed = robot.getTargetColumn();
-        robot.gyroTurning(135);
-        robot.drive(24);
+        robot.gyroTurning(-135);
         robot.drive(((cryptoBoxWidth/2) + (trackableViewed * cryptoBoxWidth)) - armOffset);
-        robot.gyroTurning(-90);
-        robot.openClamp();
+        robot.gyroTurning(90);
         robot.drive(18);
+        robot.openClamp();
     }
 }
